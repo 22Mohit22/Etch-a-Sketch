d92de126ed3ac795c8c6bb89b5a45fb8177c30cc
Mohit Pandey <mohit20020422@gmail.com>
2024-03-09 10:49:33 -0800

    Added functions to create new grids, create random rgb, to prompt for new grid. update index.html and style files accordingly

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ gridContainer.className = 'container';
 function createStartingGrid() {
 
     for(let i = 0; i < 16*16; i++) {
-        var startingBox = document.createElement('div');
+        let startingBox = document.createElement('div');
         startingBox.className = 'grid-box';
         gridContainer.appendChild(startingBox);
         startingBox.style.width = '45px';
@@ -17,9 +17,38 @@ function createStartingGrid() {
 
 gridContainer.addEventListener('mouseover', (ev) => {
     if(ev.target.classList.contains('grid-box')) {
-        ev.target.classList.add('activated');
+        ev.target.style.backgroundColor = `rgb(${createRandomRGB()})`;
     }
 })
 
-createStartingGrid();
+function createGrid(count) {
 
+    const length = (760/count) - 2.5;
+
+    for(let i = 0; i < count*count; i++) {
+        let box = document.createElement('div');
+        box.className = 'grid-box';
+        gridContainer.appendChild(box);
+        box.style.width = `${length}px`;
+        box.style.height = `${length}px`;
+    }
+    grid.appendChild(gridContainer);
+}
+
+function promptForValue() {
+    gridContainer.innerHTML = '';
+    let noOfGridsPerSide = prompt('Enter number of grids per side.\n(Not more than 100)');
+    createGrid(noOfGridsPerSide);
+}
+
+function createRandomRGB() {
+    let red, green, blue;
+
+    red = Math.round(Math.random() * 225);
+    green = Math.round(Math.random() * 225);
+    blue = Math.round(Math.random() * 225);
+
+    return `${red}, ${green}, ${blue}`;
+}
+
+createStartingGrid();
